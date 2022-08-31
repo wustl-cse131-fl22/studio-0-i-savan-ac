6b297e1bf97405184395d49097952262159f72cb
scash <scash@DESKTOP-R59IV8Q.dhcp.wustl.edu>
2022-08-31 16:46:04 -0500

    we out:P

--- src/studio0/MilageReport.java
+++ src/studio0/MilageReport.java
@@ -9,10 +9,11 @@ public class MilageReport {
 		
 		int totalMiles = car1miles + car2miles + car3miles;
 		double milesPerGallon = 24.5;
-		double totalGallons = totalMiles * milesPerGallon;
+		double totalGallons = totalMiles / milesPerGallon;
 		double pricePerGallon = 3.05;
 		double totalPrice = totalGallons * pricePerGallon;
 		
 		System.out.println("Total price spent on gas was $" + totalPrice);
 	}
 }
+  
